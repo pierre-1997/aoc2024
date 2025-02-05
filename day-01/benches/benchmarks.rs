0df096055a1086d650f8529db7b9eb8fc4c20eb6
pierre-1997 <pierre.097@proton.me>
2025-02-05 16:05:45 +0100

    Added benchmarking

---
+++ day-01/benches/benchmarks.rs
@@ -0,0 +1,21 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+pub fn bench_part_1(c: &mut Criterion) {
+    let input = include_str!("../inputs/part-1.txt");
+
+    c.bench_function("day01::part_1", |b| {
+        b.iter(|| day_01::part1::process(black_box(input)))
+    });
+}
+
+pub fn bench_part_2(c: &mut Criterion) {
+    let input = include_str!("../inputs/part-2.txt");
+
+    c.bench_function("day01::part_2", |b| {
+        b.iter(|| day_01::part2::process(black_box(input)))
+    });
+}
+
+criterion_group!(part1, bench_part_1);
+criterion_group!(part2, bench_part_2);
+criterion_main!(part1, part2);
