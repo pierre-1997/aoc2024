73da53b1e284b597504452354b404c9532c57bc6
pierre-1997 <pierre@blackfoot.io>
2025-02-10 14:10:34 +0100

    fixed template again

--- daily-template/benches/benchmarks.rs
+++ daily-template/benches/benchmarks.rs
@@ -3,16 +3,16 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 pub fn bench_part_1(c: &mut Criterion) {
     let input = include_str!("../inputs/part-1.txt");
 
-    c.bench_function("dayXX::part_1", |b| {
-        b.iter(|| daily_template::part1::process(black_box(input)))
+    c.bench_function("{{crate_name}}::part_1", |b| {
+        b.iter(|| {{crate_name}}::part1::process(black_box(input)))
     });
 }
 
 pub fn bench_part_2(c: &mut Criterion) {
     let input = include_str!("../inputs/part-2.txt");
 
-    c.bench_function("dayXX::part_2", |b| {
-        b.iter(|| daily_template::part2::process(black_box(input)))
+    c.bench_function("{{crate_name}}::part_2", |b| {
+        b.iter(|| {{crate_name}}::part2::process(black_box(input)))
     });
 }
 
